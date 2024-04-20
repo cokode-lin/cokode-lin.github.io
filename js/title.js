e1fde0a3911f66816377c1429c636c6f92ea09c0
康康 <linkk.email@qq.com>
2024-04-21 01:18:21 +0800

    Site updated: 2024-04-21 01:18:20

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
-    //离开当前页面时标签显示内容
-    document.title = '👀哪去了？？';
-    clearTimeout(titleTime);
-  } else {
-    //返回当前页面时标签显示内容
-    document.title = '🌈欢迎回来~~';
-    //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
-  }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="👀哪去了？？",clearTimeout(titleTime)):(document.title="🌈欢迎回来~~",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
