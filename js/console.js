e26a40c031d34da9dc921df7c82e5233cc51984e
cokode.lin <cokode@outlook.com>
2024-05-18 23:34:02 +0800

    Site updated: 2024-05-18 23:34:01

--- js/console.js
+++ js/console.js
@@ -1,1 +1,79 @@
-var now1=new Date;function createtime1(){var o=new Date("11/16/2021 00:00:00");now1.setTime(now1.getTime()+250);var c=(now1-o)/1e3/60/60/24,e=["欢迎来到Cokodeの个人博客🍭","要记得好好休息哦","\n                           _|                        _|            \n                           _|                        _|            \n         _|_|_|    _|_|    _|  _|      _|_|      _|_|_|    _|_|    \n       _|        _|    _|  _|_|      _|    _|  _|    _|  _|_|_|_|  \n       _|        _|    _|  _|  _|    _|    _|  _|    _|  _|        \n         _|_|_|    _|_|    _|    _|    _|_|      _|_|_|    _|_|_| \n\n","小站已经苟活",Math.floor(c),"天啦!","©2021 By Cokode"];setTimeout(console.log.bind(console,`\n%c${e[0]} %c ${e[1]} %c ${e[2]} %c${e[3]}%c ${e[4]}%c ${e[5]}\n\n%c ${e[6]}\n`,"color:#39c5bb","","color:#39c5bb","color:#39c5bb","","color:#39c5bb",""))}function createtime2(){var o=["NCC2-036","调用前置摄像头拍照成功，识别为大佬一枚","Photo captured: "," 🤪 "];setTimeout(console.log.bind(console,`%c ${o[0]} %c ${o[1]} %c \n${o[2]} %c\n${o[3]}`,"color:white; background-color:#10bcc0","","",'background:url("https://unpkg.zhimg.com/anzhiyu-assets@latest/image/common/tinggge.gif") no-repeat;font-size:450%')),setTimeout(console.log.bind(console,"%c WELCOME %c 欢迎光临","color:white; background-color:#23c682","")),setTimeout(console.warn.bind(console,"%c ⚡ Powered by Cokode %c 你正在访问Cokodeの个人博客","color:white; background-color:#f0ad4e","")),setTimeout(console.log.bind(console,"%c W23-12 %c 系统监测到你已打开控制台","color:white; background-color:#4f90d9","")),setTimeout(console.warn.bind(console,"%c S013-782 %c 你现在正处于监控中","color:white; background-color:#d9534f",""))}createtime1(),createtime2(),console.log=function(){},console.error=function(){},console.warn=function(){};
+var now1 = new Date();
+
+function createtime1() {
+    var grt = new Date("11/16/2021 00:00:00"); // 2021.11.16, 这里格式没错
+    now1.setTime(now1.getTime() + 250);
+    var days = (now1 - grt) / 1000 / 60 / 60 / 24;
+    var dnum = Math.floor(days);
+
+    var ascll = [
+        `欢迎来到Cokodeの个人博客🍭`,
+        `要记得好好休息哦`,
+        `
+                           _|                        _|            
+                           _|                        _|            
+         _|_|_|    _|_|    _|  _|      _|_|      _|_|_|    _|_|    
+       _|        _|    _|  _|_|      _|    _|  _|    _|  _|_|_|_|  
+       _|        _|    _|  _|  _|    _|    _|  _|    _|  _|        
+         _|_|_|    _|_|    _|    _|    _|_|      _|_|_|    _|_|_| 
+
+`,
+        "小站已经苟活",
+        dnum,
+        "天啦!",
+        "©2021 By Cokode",
+    ];
+
+    setTimeout(
+        console.log.bind(
+            console,
+            `\n%c${ascll[0]} %c ${ascll[1]} %c ${ascll[2]} %c${ascll[3]}%c ${ascll[4]}%c ${ascll[5]}\n\n%c ${ascll[6]}\n`,
+            "color:#39c5bb",
+            "",
+            "color:#39c5bb",
+            "color:#39c5bb",
+            "",
+            "color:#39c5bb",
+            ""
+        )
+    );
+}
+
+createtime1();
+
+function createtime2() {
+    var ascll2 = [`NCC2-036`, `调用前置摄像头拍照成功，识别为大佬一枚`, `Photo captured: `, ` 🤪 `];
+
+    setTimeout(
+        console.log.bind(
+            console,
+            `%c ${ascll2[0]} %c ${ascll2[1]} %c \n${ascll2[2]} %c\n${ascll2[3]}`,
+            "color:white; background-color:#10bcc0",
+            "",
+            "",
+            'background:url("https://unpkg.zhimg.com/anzhiyu-assets@latest/image/common/tinggge.gif") no-repeat;font-size:450%'
+        )
+    );
+
+    setTimeout(console.log.bind(console, "%c WELCOME %c 欢迎光临", "color:white; background-color:#23c682", ""));
+
+    setTimeout(
+        console.warn.bind(
+            console,
+            "%c ⚡ Powered by Cokode %c 你正在访问Cokodeの个人博客",
+            "color:white; background-color:#f0ad4e",
+            ""
+        )
+    );
+
+    setTimeout(console.log.bind(console, "%c W23-12 %c 系统监测到你已打开控制台", "color:white; background-color:#4f90d9", ""));
+    setTimeout(
+        console.warn.bind(console, "%c S013-782 %c 你现在正处于监控中", "color:white; background-color:#d9534f", "")
+    );
+}
+createtime2();
+
+// 重写console方法
+console.log = function () { };
+console.error = function () { };
+console.warn = function () { };
